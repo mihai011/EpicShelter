f5243bb2c098175b355ee945563b6d803e038e0b
mihai011 <mihaimihai011@gmail.com>
2020-02-18 22:13:40 +0200

    start of the code class using quickstart app

--- google.py
+++ google.py
@@ -1,9 +1,50 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
 
 class Google_Drive():
 
     def __init__(self):
 
-        token_location = None
-
         
+        if os.path.exists("token.pickle"):
+            with open('token.pickle', 'rb') as token:
+                self.creds = pickle.load(token)
+
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                self.creds = flow.run_local_server(port=0)
+            
+            with open('token.pickle') as token:
+                pickle.dump(self.creds,token)
+
+        self.service = build('drive', 'v3', credentials=self.creds)
+
+
+    def list_files(self, page_size):
+
+        results = self.service.files().list(pageSize=page_size, 
+            fields="nextPageToken, files(id, name)").execute()
+
+        items = results.get('files', [])
+
+        return items
+
+if __name__ == "__main__":
+
+    g = Google_Drive()
+
+    files = g.list_files(100)
+
+    print(files)
+
+
