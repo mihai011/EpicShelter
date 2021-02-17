27f52de860f0cb1ac5d57b51f4fb55fe972d1ac3
mihai011 <mihaimihai011@gmail.com>
2021-02-17 15:19:01 +0200

    Added fixture

--- epicshelter_tested/tests/test_aws_s3.py
+++ epicshelter_tested/tests/test_aws_s3.py
@@ -8,15 +8,16 @@ import boto3
 
 from .aws_utils import boto3_mock
 
-
-
-def test_source(monkeypatch):
+@pytest.fixture
+def make_mock(monkeypatch):
 
     monkeypatch.setattr(boto3, "client", boto3_mock)
+    
+
+def test_source(make_mock):
 
     # mock_client = boto3.client('s3')
     # data = mock_client.list_objects("test_bucket")
-
     my_client = S3()
 
     with pytest.raises(ValueError) as excinfo:
